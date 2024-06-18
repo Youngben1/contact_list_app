85e344fecd18fa3b950c6db7a749a8d19587bb67
Youngben1 <benjaminrenny@gmail.com>
2024-06-18 01:52:32 +0100

    contactlist section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import ContactList from './ContactList'
 import './App.css'
 
 function App() {
- 
+  const [contacts, setContacts] = useState([{firstName: 'Benjamin', lastName: 'Renny', email:'email', id: 1 }])
+
+  useEffect(() => {
+    // fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch('http://127.0.0.1:5000/contacts')
+    const data = await response.json()
+    setContacts(data.contacts)
+  }
+
+
   return (   
       <div>
-        <h1>Benjamin Renny</h1>
-      </div> 
+        <ContactList contacts={contacts}/>
+      </div>
   )
 }
 
