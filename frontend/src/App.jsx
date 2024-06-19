6b5ba94e1db4112176392520afed4c05cc232020
Youngben1 <benjaminrenny@gmail.com>
2024-06-19 15:28:22 +0100

    update and delete functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import ContactForm from "./ContactForm";
 
 function App() {
 	const [contacts, setContacts] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentContact, setCurrentContact] = useState({});
+
 
 	useEffect(() => {
 		fetchContacts()
@@ -19,23 +21,34 @@ function App() {
 
   const closeModal = () => {
     setIsModalOpen(false)
+    setCurrentContact({})
   }
 
   const openCreateModal = () => {
     if(!isModalOpen) setIsModalOpen(true)
+  }
 
+  const openEditModal = (contact) => {
+    if (isModalOpen) return 
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const handleUpdate = () => {
+    closeModal()
+    fetchContacts()
   }
 
 
 	return (
 		<>
-			<ContactList contacts={contacts} />
+			<ContactList contacts={contacts} updateContact={openEditModal} updateCallback={handleUpdate} />
       <button onClick={openCreateModal}>Create New Contact</button>
       {
         isModalOpen && <div className="modal">
           <div className="modal-content">
               <span className="close" onClick={closeModal}>&times;</span>
-            <ContactForm />
+            <ContactForm existingContact={currentContact} updateCallback={handleUpdate} />
           </div>
         </div>
       }
