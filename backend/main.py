77409e1eef74fe46a09a39f01c93685c2ed025ab
Youngben1 <benjaminrenny@gmail.com>
2024-06-19 14:26:29 +0100

    fixed bug for creating user

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ def get_contacts():
 
 @app.route('/create_contact', methods=['POST'])
 def create_contacts():
-    first_name = request.json.get('first_name')
-    last_name = request.json.get('last_name')
+    first_name = request.json.get('firstName')
+    last_name = request.json.get('lastName')
     email = request.json.get('email')
 
     if not first_name or not last_name or not email:
--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,8 @@ from config import db
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(50), unique=False, nullable=False)
-    lastName = db.Column(db.String(50), unique=False, nullable=False)
+    first_name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
 
     def to_json(self):
@@ -11,5 +11,5 @@ class Contact(db.Model):
             'id': self.id,
             'firstName': self.first_name,
             'lastName': self.last_name,
-            'email': self.email
+            'email': self.email,
         }
